52ff9855979342f990acfed03b011a2e4d49e479
Dan Nate <danpatnate@gmail.com>
2019-07-28 15:52:19 -0400

    Test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: "DAN NATE - Web Developer",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
+    author: "Dan Nate",
+    description: "Freelancer"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
