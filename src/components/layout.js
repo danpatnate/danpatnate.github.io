11eb433ca1fc2bc1ea0886fd6bc3c7e406a4183b
Dan Nate <danpatnate@gmail.com>
2019-07-28 15:18:44 -0400

    Analytics :)

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,66 +5,74 @@ import { StaticQuery, graphql } from 'gatsby'
 
 import '../assets/scss/main.scss'
 
-const Layout = ({ children, location }) => {
 
-  let content;
-
-  if (location && location.pathname === '/') {
-    content = (
-      <div>
-        {children}
-      </div>
-    )
-  } else {
-    content = (
-      <div id="wrapper" className="page">
-        <div>
-          {children}
-        </div>
-      </div>
-    )
+class Layout extends React.Component {
+  constructor(props) {
+    super(props)
   }
 
-  return (
-    <StaticQuery
-      query={graphql`
-        query SiteTitleQuery {
-          site {
-            siteMetadata {
-              title
+  componentDidMount() {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-144720509-1');
+  }
+
+  render () {
+    let content;
+
+    if (this.props.location && this.props.location.pathname === '/') {
+      content = (
+        <div>
+          {this.props.children}
+        </div>
+      )
+    } else {
+      content = (
+        <div id="wrapper" className="page">
+          <div>
+            {this.props.children}
+          </div>
+        </div>
+      )
+    }
+
+    return (
+      <StaticQuery
+        query={graphql`
+          query SiteTitleQuery {
+            site {
+              siteMetadata {
+                title
+              }
             }
           }
-        }
-      `}
-      render={data => (
-        <>
-          <Helmet
-            title={data.site.siteMetadata.title}
-            meta={[
-              { name: 'description', content: 'Dan Nate - Web Developer' },
-              { name: 'keywords', content: 'freelance, contractor, contract, web design, web developer, js, javascript, react' },
-            ]}
-          >
-            <html lang="en" />
-            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-144720509-1"></script>
-            <script>
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', 'UA-144720509-1');
-            </script>
-            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous" />
-          </Helmet>
-          {content}
-        </>
-      )}
-    />
-  )
+        `}
+        render={data => (
+          <>
+            <Helmet
+              title={data.site.siteMetadata.title}
+              meta={[
+                { name: 'description', content: 'Dan Nate - Web Developer' },
+                { name: 'keywords', content: 'freelance, contractor, contract, web design, web developer, js, javascript, react' },
+              ]}
+            >
+              <html lang="en" />
+              <script async src="https://www.googletagmanager.com/gtag/js?id=UA-144720509-1"></script>
+              <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous" />
+            </Helmet>
+            {content}
+          </>
+        )}
+      />
+    )
+  }
 }
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
+  location: PropTypes.object
 }
 
 export default Layout
