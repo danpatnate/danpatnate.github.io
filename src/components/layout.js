4f6db52e5fae37e29ff185fcb49662be3df78049
Dan Nate <danpatnate@gmail.com>
2019-07-28 15:39:01 -0400

    Analytics :(

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,67 +5,58 @@ import { StaticQuery, graphql } from 'gatsby'
 
 import '../assets/scss/main.scss'
 
+const Layout = ({ children, location }) => {
 
-class Layout extends React.Component {
-  constructor(props) {
-    super(props)
-  }
+  let content;
 
-  componentDidMount() {
-  }
-
-  render () {
-    let content;
-
-    if (this.props.location && this.props.location.pathname === '/') {
-      content = (
+  if (location && location.pathname === '/') {
+    content = (
+      <div>
+        {children}
+      </div>
+    )
+  } else {
+    content = (
+      <div id="wrapper" className="page">
         <div>
-          {this.props.children}
+          {children}
         </div>
-      )
-    } else {
-      content = (
-        <div id="wrapper" className="page">
-          <div>
-            {this.props.children}
-          </div>
-        </div>
-      )
-    }
-
-    return (
-      <StaticQuery
-        query={graphql`
-          query SiteTitleQuery {
-            site {
-              siteMetadata {
-                title
-              }
-            }
-          }
-        `}
-        render={data => (
-          <>
-            <Helmet
-              title={data.site.siteMetadata.title}
-              meta={[
-                { name: 'description', content: 'Dan Nate - Web Developer' },
-                { name: 'keywords', content: 'freelance, contractor, contract, web design, web developer, js, javascript, react' },
-              ]}
-            >
-              <html lang="en" />
-            </Helmet>
-            {content}
-          </>
-        )}
-      />
+      </div>
     )
   }
+
+  return (
+    <StaticQuery
+      query={graphql`
+        query SiteTitleQuery {
+          site {
+            siteMetadata {
+              title
+            }
+          }
+        }
+      `}
+      render={data => (
+        <>
+          <Helmet
+            title={data.site.siteMetadata.title}
+            meta={[
+              { name: 'description', content: 'Dan Nate - Web Developer' },
+              { name: 'keywords', content: 'freelance, contractor, contract, web design, web developer, js, javascript, react' },
+            ]}
+          >
+            <html lang="en" />
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous" />
+          </Helmet>
+          {content}
+        </>
+      )}
+    />
+  )
 }
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-  location: PropTypes.object
 }
 
 export default Layout
